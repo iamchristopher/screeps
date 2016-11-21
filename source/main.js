bef935a4be4db7c075eb59585e45843144c3977f
Chris Wright <chris@arcane.ws>
2016-11-20 22:11:26 -0500

    Added mining role logic

--- source/main.js
+++ source/main.js
@@ -1,22 +1,21 @@
-import {
-    mine
-} from './roles/mine';
+import * as mine from './roles/mine';
 
 export default {
     loop () {
         for (let i in Game.creeps) {
             const creep = Game.creeps[i];
 
-            if (creep.carryCapacity > CARRY_CAPACITY) {
+            if (creep.carry.mineralAmount < creep.carryCapacity) {
                 continue;
             }
-mine()
-            const mineral = Game.rooms['W28N66']
-                .find(FIND_MINERALS)[0];
-            creep.moveTo(mineral);
 
-            // const minerals = room.find(FIND_MINERALS);
-            // console.log(minerals);
+            const target = creep.room
+                .find(FIND_SOURCES)[0];
+
+            mine.collectResource({
+                creep,
+                target
+            });
         }
     }
 }
--- source/roles/mine.js
+++ source/roles/mine.js
@@ -1,3 +1,12 @@
-export const mine = () => {
-    console.log('mine');
+export const collectResource = ({
+    creep,
+    target
+} = {}) => {
+    if (creep.harvest(target) === ERR_NOT_IN_RANGE) {
+        return creep.moveTo(target);
+    }
+
+    if (creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+        creep.moveTo(Game.spawns['Spawn1']);
+    }
 }
