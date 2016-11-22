8cdf2ef20423d3f7d9a5d9e2e5b30ccbd2da5d20
Chris Wright <chris@arcane.ws>
2016-11-22 18:44:11 -0500

    Changed creep spawn names to contain role

--- source/roles/spawn.js
+++ source/roles/spawn.js
@@ -17,20 +17,17 @@ export default {
                         :   1
                 };
             }, {});
-// console.log(JSON.stringify(currentSpawns));
-        if (!currentSpawns.harvester || currentSpawns.harvester < 2) {
-            // console.log('spawn harvester', spawnName);
-            spawner.createCreep([ WORK, MOVE, CARRY ], spawnName, {
+
+        if (!currentSpawns.harvester || currentSpawns.harvester < 3) {
+            spawner.createCreep([ WORK, MOVE, CARRY ], `Harvester_${Game.time}`, {
                 role: 'harvester'
             });
-        } else if (!currentSpawns.upgrader || currentSpawns.upgrader < 1) {
-            console.log('spawn upgrader', spawnName);
-            spawner.createCreep([ WORK, MOVE, CARRY ], spawnName, {
+        } else if (!currentSpawns.upgrader || currentSpawns.upgrader < 2) {
+            spawner.createCreep([ WORK, MOVE, CARRY ], `Upgrader_${Game.time}`, {
                 role: 'upgrader'
             });
         } else if (!currentSpawns.miner || currentSpawns.miner < 1) {
-            console.log('spawn miner', spawnName);
-            spawner.createCreep([ MOVE, WORK, WORK ], spawnName, {
+            spawner.createCreep([ MOVE, WORK, CARRY ], `Miner_${Game.time}`, {
                 role: 'miner'
             });
         }
