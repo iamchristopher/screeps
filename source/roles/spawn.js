971897b477605a7e228ad8a6735d9e3133232978
Chris Wright <chris@arcane.ws>
2016-11-24 18:56:21 -0500

    Changed creep spawns can now scale with available enery cap

--- source/roles/spawn.js
+++ source/roles/spawn.js
@@ -4,6 +4,12 @@ export default {
             return;
         }
 
+        const {
+            room: {
+                energyCapacityAvailable
+            }
+        } = spawner;
+
         const spawnName = `Spawn_${Game.time}`;
 
         const currentSpawns = Object.keys(Game.creeps)
@@ -18,20 +24,40 @@ export default {
                 };
             }, {});
 
-        if (!currentSpawns.harvester || currentSpawns.harvester < 2) {
-            spawner.createCreep([ WORK, WORK, MOVE, CARRY ], `Harvester_${Game.time}`, {
+        if (!currentSpawns.harvester || currentSpawns.harvester < 7) {
+            let workEthic = [ WORK, MOVE, CARRY ];
+            if (energyCapacityAvailable > 300) {
+                workEthic = [ WORK, WORK, MOVE, CARRY ];
+            }
+
+            spawner.createCreep(workEthic, `Harvester_${Game.time}`, {
                 role: 'harvester'
             });
         } else if (!currentSpawns.upgrader || currentSpawns.upgrader < 5) {
-            spawner.createCreep([ WORK, WORK, MOVE, CARRY ], `Upgrader_${Game.time}`, {
+            let workEthic = [ WORK, MOVE, CARRY ];
+            if (energyCapacityAvailable > 300) {
+                workEthic = [ WORK, WORK, MOVE, CARRY ];
+            }
+
+            spawner.createCreep(workEthic, `Upgrader_${Game.time}`, {
                 role: 'upgrader'
             });
         } else if (!currentSpawns.builder || currentSpawns.builder < 4) {
-            spawner.createCreep([ WORK, WORK, MOVE, CARRY ], `Builder_${Game.time}`, {
+            let workEthic = [ WORK, MOVE, CARRY ];
+            if (energyCapacityAvailable > 300) {
+                workEthic = [ WORK, WORK, MOVE, CARRY ];
+            }
+
+            spawner.createCreep(workEthic, `Builder_${Game.time}`, {
                 role: 'builder'
             });
         } else if (!currentSpawns.miner || currentSpawns.miner < 0) {
-            spawner.createCreep([ MOVE, WORK, CARRY ], `Miner_${Game.time}`, {
+            let workEthic = [ WORK, MOVE, CARRY ];
+            if (energyCapacityAvailable > 300) {
+                workEthic = [ WORK, WORK, MOVE, CARRY ];
+            }
+
+            spawner.createCreep(workEthic, `Miner_${Game.time}`, {
                 role: 'miner'
             });
         }
