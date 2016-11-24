fac4afe5e03f53e7c5e1c843741053daaa5fe4ad
Chris Wright <chris@arcane.ws>
2016-11-23 20:07:43 -0500

    Changed builders now prefer to complete in-progress structures and then fallback to preference chain

--- source/roles/build.js
+++ source/roles/build.js
@@ -13,10 +13,21 @@ export default {
         if (creep.memory.building) {
             const sites = creep.room.find(FIND_CONSTRUCTION_SITES);
 
+            const preference = [
+                STRUCTURE_CONTAINER,
+                STRUCTURE_WALL
+            ].reverse();
+
             if (sites) {
                 const targetSites = sites.sort(sortSitesByProgress);
                 const inProgressSite = targetSites.find(s => s.progress > 0);
-                const targetSite = inProgressSite || creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
+
+                let targetSite;
+                if (inProgressSite) {
+                    targetSite = inProgressSite;
+                } else {
+                    targetSite = sites.sort(sortByPreference(preference)).reverse()[0];
+                }
 
                 if (creep.build(targetSite) === ERR_NOT_IN_RANGE) {
                     creep.moveTo(targetSite);
@@ -29,6 +40,31 @@ export default {
         }
     }
 };
+
+function sortByPreference (preference) {
+    return (a, b) => {
+        const x = preference.indexOf(a.structureType);
+        const y = preference.indexOf(b.structureType);
+
+        if (x > 0 && y < 0) {
+            return 1;
+        }
+
+        if (x < 0 && y > 0) {
+            return -1;
+        }
+
+        if (x > y) {
+            return 1;
+        }
+
+        if (x < y) {
+            return -1;
+        }
+
+        return 0;
+    };
+}
 
 function sortSitesByProgress (a, b) {
     if (a.progress < b.progress) {
