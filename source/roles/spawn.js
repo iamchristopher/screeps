745bc916fb267d7e0a32e98465f6b9df7315041a
Chris Wright <chris@arcane.ws>
2016-11-24 07:46:42 -0500

    Tweaked spawning numbers

--- source/roles/spawn.js
+++ source/roles/spawn.js
@@ -18,21 +18,21 @@ export default {
                 };
             }, {});
 
-        if (!currentSpawns.harvester || currentSpawns.harvester < 0) {
-            spawner.createCreep([ WORK, MOVE, CARRY ], `Harvester_${Game.time}`, {
+        if (!currentSpawns.harvester || currentSpawns.harvester < 2) {
+            spawner.createCreep([ WORK, WORK, MOVE, CARRY ], `Harvester_${Game.time}`, {
                 role: 'harvester'
             });
-        } else if (!currentSpawns.upgrader || currentSpawns.upgrader < 3) {
-            spawner.createCreep([ WORK, MOVE, CARRY ], `Upgrader_${Game.time}`, {
+        } else if (!currentSpawns.upgrader || currentSpawns.upgrader < 5) {
+            spawner.createCreep([ WORK, WORK, MOVE, CARRY ], `Upgrader_${Game.time}`, {
                 role: 'upgrader'
             });
-        } else if (!currentSpawns.miner || currentSpawns.miner < 1) {
+        } else if (!currentSpawns.builder || currentSpawns.builder < 4) {
+            spawner.createCreep([ WORK, WORK, MOVE, CARRY ], `Builder_${Game.time}`, {
+                role: 'builder'
+            });
+        } else if (!currentSpawns.miner || currentSpawns.miner < 0) {
             spawner.createCreep([ MOVE, WORK, CARRY ], `Miner_${Game.time}`, {
                 role: 'miner'
-            });
-        } else if (!currentSpawns.builder || currentSpawns.builder < 1) {
-            spawner.createCreep([ WORK, MOVE, CARRY ], `Builder_${Game.time}`, {
-                role: 'builder'
             });
         }
     }
