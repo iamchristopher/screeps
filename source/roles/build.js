ff259d852a9ddb50a52e4243c7a04086f7f4b2ab
Chris Wright <chris@arcane.ws>
2016-11-25 18:15:55 -0500

    Fixed build role preference order hack

--- source/roles/build.js
+++ source/roles/build.js
@@ -1,5 +1,11 @@
 import harvest from './harvest';
 
+const preferredStructures = [
+    STRUCTURE_EXTENSION,
+    STRUCTURE_CONTAINER,
+    STRUCTURE_WALL
+];
+
 export default {
     run (creep) {
         if (creep.memory.building && creep.carry.energy === 0) {
@@ -13,12 +19,6 @@ export default {
         if (creep.memory.building) {
             const sites = creep.room.find(FIND_CONSTRUCTION_SITES);
 
-            const preference = [
-                STRUCTURE_CONTAINER,
-                STRUCTURE_EXTENSION,
-                STRUCTURE_WALL
-            ].reverse();
-
             if (sites) {
                 const targetSites = sites.sort(sortSitesByProgress);
                 const inProgressSite = targetSites.find(s => s.progress > 0);
@@ -27,7 +27,7 @@ export default {
                 if (inProgressSite) {
                     targetSite = inProgressSite;
                 } else {
-                    targetSite = sites.sort(sortByPreference(preference)).reverse()[0];
+                    targetSite = sites.sort(sortByPreference(preferredStructures))[0];
                 }
 
                 if (creep.build(targetSite) === ERR_NOT_IN_RANGE) {
@@ -51,25 +51,25 @@ export default {
     }
 };
 
-function sortByPreference (preference) {
+function sortByPreference (preferences) {
     return (a, b) => {
-        const x = preference.indexOf(a.structureType);
-        const y = preference.indexOf(b.structureType);
+        const x = preferences.indexOf(a.structureType);
+        const y = preferences.indexOf(b.structureType);
 
         if (x > 0 && y < 0) {
-            return 1;
+            return -1;
         }
 
         if (x < 0 && y > 0) {
-            return -1;
-        }
-
-        if (x > y) {
             return 1;
         }
 
-        if (x < y) {
+        if (x > y) {
             return -1;
+        }
+
+        if (x < y) {
+            return 1;
         }
 
         return 0;
