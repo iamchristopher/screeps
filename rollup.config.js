1533a2c3c5c1e47cc5178a5c75f80fc123cec447
Chris Wright <chris@arcane.ws>
2016-11-28 08:40:33 -0500

    Updated build process to handle node_modules correctly

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,20 @@
 import path from 'path';
 import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 
 export default {
     entry: 'source/main.js',
     format: 'cjs',
     plugins: [
-        babel()
+        babel({
+            exclude: 'node_modules/**'
+        }),
+        resolve({
+            jsnext: true,
+            main: true
+        }),
+        commonjs()
     ],
     dest: 'main.js'
 }
