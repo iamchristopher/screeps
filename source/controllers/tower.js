a5e2247cff92577756dd7d31920753497a898a85
Chris Wright <chris@arcane.ws>
2016-12-01 19:11:43 -0500

    Added preferred structure repairs to tower

--- source/controllers/tower.js
+++ source/controllers/tower.js
@@ -1,10 +1,45 @@
 import {
     tickThrottle
 } from '../utils/game';
+import {
+    byPreference
+} from '../utils/sort';
+
+const preferredStructures = [
+    STRUCTURE_SPAWN,
+    STRUCTURE_CONTROLLER,
+    STRUCTURE_CONTAINER,
+    STRUCTURE_EXTENSION,
+    STRUCTURE_RAMPART,
+    STRUCTURE_ROAD,
+    STRUCTURE_WALL
+];
 
 export default (tower) => {
-    const target = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-    if (target) {
-        return tower.attack(target);
+    const attackTarget = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+    if (attackTarget) {
+        return tower.attack(attackTarget);
     }
+
+    const repairTarget = tower.room.find(FIND_STRUCTURES, {
+        filter (o) {
+            if ([ STRUCTURE_WALL, STRUCTURE_RAMPART ].indexOf(o.structureType) > -1) {
+                if (o.hits < 50000) {
+                    return true;
+                }
+
+                return false;
+            }
+
+            if (o.hits < o.hitsMax) {
+                return true;
+            }
+
+            return false;
+        }
+    })
+        .sort(byPreference(preferredStructures))
+        .shift();
+
+    tower.repair(repairTarget);
 }
