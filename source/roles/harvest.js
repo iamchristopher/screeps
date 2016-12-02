fff214f2b90135ed33839c4d3af72af332b20666
Chris Wright <chris@arcane.ws>
2016-12-01 21:04:27 -0500

    Changed harvesters no longer pole for targets

--- source/roles/harvest.js
+++ source/roles/harvest.js
@@ -14,33 +14,32 @@ const preferredStructures = [
 export default {
     run (creep) {
         if (creep.memory.working && creep.carry.energy === 0) {
+            const target = creep.pos.findClosestByRange(FIND_SOURCES);
+            creep.memory.target = target.id;
             creep.memory.working = false;
         }
 
         if (!creep.memory.working && creep.carry.energy === creep.carryCapacity) {
+            const target = creep.room.find(FIND_STRUCTURES)
+                .sort(byPreference(preferredStructures))
+                .filter(structure => {
+                    if (structure.energy < structure.energyCapacity) {
+                        return true;
+                    }
+
+                    if (structure.store && structure.store.energy < structure.storeCapacity) {
+                        return true;
+                    }
+
+                    return false;
+                })
+                .shift();
+
+            creep.memory.target = target.id;
             creep.memory.working = true;
         }
 
         if (creep.memory.working) {
-            if (tickThrottle(10)) {
-                const target = creep.room.find(FIND_STRUCTURES)
-                    .sort(byPreference(preferredStructures))
-                    .filter(structure => {
-                        if (structure.energy < structure.energyCapacity) {
-                            return true;
-                        }
-
-                        if (structure.store && structure.store.energy < structure.storeCapacity) {
-                            return true;
-                        }
-
-                        return false;
-                    })
-                    .shift();
-
-                creep.memory.target = target.id;
-            }
-
             if (creep.memory.target) {
                 const harvestTarget = Game.getObjectById(creep.memory.target);
 
@@ -49,14 +48,11 @@ export default {
                 }
             }
         } else {
-            if (tickThrottle(10)) {
-                const target = creep.pos.findClosestByRange(FIND_SOURCES);
-                creep.memory.target = target.id;
-            }
-
-            const harvestTarget = Game.getObjectById(creep.memory.target);
-            if (harvestTarget && creep.harvest(harvestTarget) === ERR_NOT_IN_RANGE) {
-                creep.moveTo(harvestTarget);
+            if (creep.memory.target) {
+                const harvestTarget = Game.getObjectById(creep.memory.target);
+                if (harvestTarget && creep.harvest(harvestTarget) === ERR_NOT_IN_RANGE) {
+                    creep.moveTo(harvestTarget);
+                }
             }
         }
     }
