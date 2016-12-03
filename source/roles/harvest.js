635f1c7f358067577b6f3cc1594016bf8f81a0d6
Chris Wright <chris@arcane.ws>
2016-12-03 13:10:29 -0500

    Optimised harvest role to only calculate targets when needed

--- source/roles/harvest.js
+++ source/roles/harvest.js
@@ -13,46 +13,79 @@ const preferredStructures = [
 
 export default {
     run (creep) {
-        if (creep.memory.working && creep.carry.energy === 0) {
-            const target = creep.pos.findClosestByRange(FIND_SOURCES);
-            creep.memory.target = target.id;
-            creep.memory.working = false;
-        }
-
-        if (!creep.memory.working && creep.carry.energy === creep.carryCapacity) {
-            const target = creep.room.find(FIND_STRUCTURES)
-                .sort(byPreference(preferredStructures))
-                .filter(structure => {
-                    if (structure.energy < structure.energyCapacity) {
-                        return true;
-                    }
-
-                    if (structure.store && structure.store.energy < structure.storeCapacity) {
-                        return true;
-                    }
-
-                    return false;
-                })
-                .shift();
-
-            creep.memory.target = target.id;
+        if (!creep.memory.working && creep.carry.energy === 0) {
+            creep.memory.target = null;
             creep.memory.working = true;
         }
 
-        if (creep.memory.working) {
-            if (creep.memory.target) {
-                const harvestTarget = Game.getObjectById(creep.memory.target);
+        if (creep.memory.working && creep.carry.energy === creep.carryCapacity) {
+            creep.memory.target = null;
+            creep.memory.working = false;
+        }
 
-                if (harvestTarget && creep.transfer(harvestTarget, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
-                    creep.moveTo(harvestTarget);
-                }
+        if (creep.memory.working) {
+            if (!creep.memory.target) {
+                const target = creep.pos.findClosestByRange(FIND_SOURCES);
+
+                creep.memory.target = target.id;
+            }
+
+            const target = Game.getObjectById(creep.memory.target);
+            const actionResult = creep.harvest(target);
+
+            switch (actionResult) {
+                case ERR_NOT_IN_RANGE:
+                    creep.moveTo(target);
+                    break;
+                case ERR_INVALID_TARGET:
+                    console.log('Invalid harvest target', target);
+                    creep.memory.target = null;
+                    break;
+                case OK:
+                    // S'all good
+                    break;
+                default:
+                    console.log('No harvest action for result', actionResult);
             }
         } else {
-            if (creep.memory.target) {
-                const harvestTarget = Game.getObjectById(creep.memory.target);
-                if (harvestTarget && creep.harvest(harvestTarget) === ERR_NOT_IN_RANGE) {
-                    creep.moveTo(harvestTarget);
-                }
+            if (!creep.memory.target) {
+                const target = creep.room.find(FIND_STRUCTURES, {
+                    filter (o) {
+                        if (o.energy < o.energyCapacity) {
+                            return true;
+                        }
+
+                        if (o.store && o.store.energy < o.storeCapacity) {
+                            return true;
+                        }
+
+                        return false;
+                    }
+                })
+                    .sort(byPreference(preferredStructures))
+                    .shift();
+
+                creep.memory.target = target.id;
+            }
+
+            const target = Game.getObjectById(creep.memory.target);
+            const actionResult = creep.transfer(target, RESOURCE_ENERGY);
+
+            switch (actionResult) {
+                case ERR_NOT_IN_RANGE:
+                    creep.moveTo(target);
+                    break;
+                case ERR_FULL:
+                case ERR_INVALID_TARGET:
+                case ERR_NOT_ENOUGH_ENERGY:
+                    console.log('Invalid deposit target', target);
+                    creep.memory.target = null;
+                    break;
+                case OK:
+                    // S'all good
+                    break;
+                default:
+                    console.log('No deposit action for result', actionResult);
             }
         }
     }
