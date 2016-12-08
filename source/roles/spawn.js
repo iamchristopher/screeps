0af3e320c089fdc314d1e1b5f975d47802528774
Chris Wright <chris@arcane.ws>
2016-12-08 08:43:57 -0500

    Tweaked spawning

--- source/roles/spawn.js
+++ source/roles/spawn.js
@@ -26,60 +26,53 @@ export default {
             }, {});
 
         if (!currentSpawns.harvester || currentSpawns.harvester < 3) {
-            let workEthic = [ WORK, MOVE, CARRY ];
-            if (energyCapacityAvailable >= 500) {
+            let workEthic = [ WORK, WORK, MOVE, CARRY ];
+            if (energyCapacityAvailable >= 900) {
+                workEthic = [ MOVE, MOVE, MOVE, MOVE, WORK, WORK, WORK, WORK, WORK, CARRY, CARRY ];
+            } else if (energyCapacityAvailable >= 500) {
                 workEthic = [ WORK, WORK, WORK, MOVE, MOVE, CARRY, CARRY ];
             } else if (energyCapacityAvailable >= 400) {
                 workEthic = [ WORK, WORK, WORK, MOVE, CARRY ];
-            } else if (energyCapacityAvailable >= 300) {
-                workEthic = [ WORK, WORK, MOVE, CARRY ];
             }
 
             spawner.createCreep(workEthic, `Harvester_${Game.time}`, {
                 role: 'harvester'
             });
         } else if (!currentSpawns.upgrader || currentSpawns.upgrader < 3) {
-            let workEthic = [ WORK, MOVE, CARRY ];
+            let workEthic = [ WORK, WORK, MOVE, CARRY ];
             if (energyCapacityAvailable >= 400) {
-                workEthic = [ WORK, WORK, MOVE, CARRY, CARRY ];
-            } else if (energyCapacityAvailable >= 300) {
-                workEthic = [ WORK, WORK, MOVE, CARRY ];
+                workEthic = [ WORK, WORK, MOVE, MOVE, CARRY, CARRY ];
             }
 
             spawner.createCreep(workEthic, `Upgrader_${Game.time}`, {
                 role: 'upgrader'
             });
         } else if (!currentSpawns.builder || currentSpawns.builder < 1) {
-            let workEthic = [ WORK, MOVE, CARRY ];
+            let workEthic = [ WORK, WORK, MOVE, CARRY ];
             if (energyCapacityAvailable >= 500) {
                 workEthic = [ WORK, WORK, WORK, MOVE, CARRY, CARRY, CARRY ];
             } else if (energyCapacityAvailable >= 400) {
                 workEthic = [ WORK, WORK, WORK, MOVE, CARRY ];
-            } else if (energyCapacityAvailable >= 300) {
-                workEthic = [ WORK, WORK, MOVE, CARRY ];
             }
 
             spawner.createCreep(workEthic, `Builder_${Game.time}`, {
                 role: 'builder'
             });
-        } else if (!currentSpawns.maintainer || currentSpawns.maintainer < 2) {
-            let workEthic = [ WORK, MOVE, CARRY ];
-            if (energyCapacityAvailable >= 500) {
+        } else if (!currentSpawns.maintainer || currentSpawns.maintainer < 3) {
+            let workEthic = [ WORK, WORK, MOVE, CARRY ];
+            if (energyCapacityAvailable >= 600) {
+                workEthic = [ WORK, WORK, WORK, WORK, MOVE, MOVE, CARRY, CARRY ];
+            } else if (energyCapacityAvailable >= 500) {
                 workEthic = [ WORK, WORK, WORK, WORK, MOVE, CARRY ];
             } else if (energyCapacityAvailable >= 400) {
                 workEthic = [ WORK, WORK, WORK, MOVE, CARRY ];
-            } else if (energyCapacityAvailable >= 300) {
-                workEthic = [ WORK, WORK, MOVE, CARRY ];
             }
 
             spawner.createCreep(workEthic, `Maintainer_${Game.time}`, {
                 role: 'maintainer'
             });
         } else if (!currentSpawns.hauler || currentSpawns.hauler < 1) {
-            let workEthic = [ WORK, MOVE, CARRY ];
-            if (energyCapacityAvailable >= 300) {
-                workEthic = [ WORK, MOVE, MOVE, CARRY, CARRY ];
-            }
+            let workEthic = [ WORK, MOVE, MOVE, CARRY, CARRY ];
 
             spawner.createCreep(workEthic, `Hauler_${Game.time}`, {
                 role: 'hauler'
