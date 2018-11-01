5db03eff352f5a7c9eee967df5f5ed5d22c8037a
iamchristopher <chriswright@live.com>
2018-11-01 13:12:39 -0400

    Updated build process

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
-import path from 'path';
 import babel from 'rollup-plugin-babel';
-import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
 
 export default {
     entry: 'source/main.js',
